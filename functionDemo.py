c775107d3cf2171195aefa8213449cc6422c9d1e
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-10-16 14:30:21 -0400

    Updated from Brython Server: 10/16/2017 2:30:21 PM

--- functionDemo.py
+++ functionDemo.py
@@ -1,1 +1,18 @@
+#Morgan Baughman
+#10/16/17 
+#functionsDemo.py - learning functions 
+def hw(): 
+    print('Hello, World') 
+    
+def bigger(num1, num2): #prints which number is bigger. 
+if num1 > num2: 
+    print(num1) 
+else: print(num2) 
 
+def slope(x1, x2, y1, y2): #calculate slope 
+    print((y2-y1)/(x2-x1)) 
+
+bigger(13, 27) #test1 
+bigger(-10,-15) #test2
+bigger('Morgan', 'Greg') 
+slope(1,2,2,4)
