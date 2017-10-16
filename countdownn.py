0a97da7dd0de073b59c1e6331642f0c915cbf711
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-10-16 14:48:44 -0400

    Updated from Brython Server: 10/16/2017 2:48:44 PM

--- countdownn.py
+++ countdownn.py
@@ -3,8 +3,10 @@
 #countdown.py - Countdown to BOOM!
 
 
-def countdown():
-    for i in range (-5,0):
+def countdown(num):
+    for i in range (-num,0):
       print(-i)
       if -i == 1:
         print('BOOM!')
+
+countdown(5)
