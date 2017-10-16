1fdb75209fa7abbb395109bd239af6a32e419037
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-10-16 14:34:33 -0400

    Updated from Brython Server: 10/16/2017 2:34:32 PM

--- rectangle.py
+++ rectangle.py
@@ -1,1 +1,13 @@
+#Morgan Baughman
+#10/16/17
+#rectangle.py 
 
+def area(num1, num2):
+    print(num1*num2)
+
+area(4,5)
+
+def perimeter(num1,num2):
+    print((num1*2)+(num2*2))
+    
+perimeter(4,5)
