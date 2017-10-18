43b0bcfa60db490c3cef170c2dc7fd20a6c52a59
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-10-18 13:35:27 -0400

    Updated from Brython Server: 10/18/2017 1:35:26 PM

--- returnDemo.py
+++ returnDemo.py
@@ -1,1 +1,13 @@
+#MorganBaughman
+#10/18/17
+#returnDemo.py
 
+from random import randint
+
+def randevenint(low,high):
+    return randint(low,high)
+
+r1 = randevenint(1,100)
+r2 = randevenint(1,100)
+r3 = randevenint(1,100)
+print(r1, r2, r3)
