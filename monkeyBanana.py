3b8f8c32063cb8bf28781199da1949aef7c8c5fa
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-10-19 14:53:59 -0400

    Updated from Brython Server: 10/19/2017 2:53:57 PM

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -5,33 +5,56 @@ from random import randint
 from ggame import *
 
 #constants
-ROWS = 40
-COLS = 50
+ROWS = 20
+COLS = 40
 CELL_SIZE = 20
 
 def moveRight(event):
     if monkey.x < (COLS-1)*CELL_SIZE:
         monkey.x += CELL_SIZE
+        if monkey.x == banana.x and monkey.y == banana.y:
+            moveBanana()
+            updateScore()
     
 def moveLeft(event):
      if monkey.x > 0:
         monkey.x -= CELL_SIZE
+        if monkey.x == banana.x and monkey.y == banana.y:
+            moveBanana()
+            updateScore()
     
 def moveUp(event):
     if monkey.y > 0:
         monkey.y -= CELL_SIZE
+        if monkey.x == banana.x and monkey.y == banana.y:
+            moveBanana()
+            updateScore()
     
 def moveDown(event):
     monkey.y += CELL_SIZE
+    if monkey.x == banana.x and monkey.y == banana.y:
+            moveBanana()
+            updateScore()
 
 def moveBanana():
     banana.x = randint(0,COLS-1)*CELL_SIZE
     banana.y = randint(0,ROWS-1)*CELL_SIZE
 
+def updateScore():
+    data['score'] += 100
+    data['scoreText'].destroy()
+    scoreBox = TextAsset('Score = '+str(data['score']))
+    data['scoreText'] = Sprite(scoreBox,(0,ROWS*CELL_SIZE))
 
 
 
 if __name__ == '__main__':
+    
+    data = {}
+    data['score'] = 0
+    
+    
+    
     
     green = Color(0x006600, 1)
     brown = Color(0x8B4513, 1)
@@ -40,10 +63,15 @@ if __name__ == '__main__':
     jungleBox = RectangleAsset(COLS*CELL_SIZE, ROWS*CELL_SIZE, LineStyle(1, green), green)
     monkeyBox = RectangleAsset(CELL_SIZE, CELL_SIZE,LineStyle(1, brown), brown)
     bananaBox = RectangleAsset(CELL_SIZE, CELL_SIZE,LineStyle(1, yellow), yellow)
+    scoreBox = TextAsset('Score = 0')
+        
+        
         
     Sprite(jungleBox)
     banana = Sprite(bananaBox,(COLS*CELL_SIZE/2, ROWS*CELL_SIZE/2))
     monkey = Sprite(monkeyBox)
+    data['scoreText'] = Sprite(scoreBox,(0,ROWS*CELL_SIZE))
+    
     
     App().listenKeyEvent('keydown' ,'right arrow' ,moveRight)
     App().listenKeyEvent('keydown' ,'left arrow' ,moveLeft)
