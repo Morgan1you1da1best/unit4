787d0abb64d6e965505a5e1edf278b0eb9d5a4a0
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-10-25 14:01:38 -0400

    Updated from Brython Server: 10/25/2017 2:01:37 PM

--- stringUnion.py
+++ stringUnion.py
@@ -1,1 +1,18 @@
+#MorganBaughman
+#10/25/17
+#stringUnion.py
 
+def stringUnion(word1, word2):
+    total = " "
+    for ch in word1:
+        if not ch in total:
+            total += ch
+            
+    for ch in word2:
+        if not ch in total:
+            total += ch
+            
+    return total
+
+total = stringUnion("mississippi", "pennsylvania")
+print(total)
