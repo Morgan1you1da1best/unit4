18695b869d97c1415d77e55f758643b851040038
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-10-26 14:13:04 -0400

    Updated from Brython Server: 10/26/2017 2:13:03 PM

--- recursionDemo.py
+++ recursionDemo.py
@@ -1,1 +1,14 @@
+#Morgan Baughman
+#10/26/17
+#recursionDemo.py - recursive version of countdown function
+
+def countdown(n):
+    if n == 0:
+      print('Boom!')
+    else:
+        print(n)
+        countdown(n-1)
+        
+countdown(5)
+
 
