b7c1eaa6ab56915e69c97d6ecb9a4b97a46f4a88
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-10-27 14:36:33 -0400

    Updated from Brython Server: 10/27/2017 2:36:33 PM

--- warmUp.py
+++ warmUp.py
@@ -1,1 +1,11 @@
+#Morgan Baughman
+#10/27/17
+#warmUp.py - figure out the GCF of two numbers
 
+
+def GCF(num1,num2):
+    for i in range(num1,0,-1):
+        if num1%i == 0 and num2%i == 0:
+            return(i)
+
+print(GCF(10,15))
