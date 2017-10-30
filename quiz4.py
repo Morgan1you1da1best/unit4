3fc689f7b6635de9c71cb9723e81cf3e1a43f69f
Morgan1you1da1best <Morganbaughman@hanovernorwichschools.org>
2017-10-30 14:56:01 -0400

    Updated from Brython Server: 10/30/2017 2:56:01 PM

--- quiz4.py
+++ quiz4.py
@@ -1,1 +1,20 @@
+#Morgan Baughman
+#10/30/17
+#quiz4.py
 
+def csia(num):
+    for i in range (0,num):
+        print('Computer Science is Awesome')
+def same(num1, num2):
+    if num1 == num2:
+        return('True')
+def average(num1,num2,num3):
+    print((num1+num2+num3)/3)
+def lastLetter(word):
+        if 'f' in word:
+            print('f')
+
+csia(5)
+average(1,2,3)
+lastLetter('Smedinghoff')
+print(same(3*5,19-4))
